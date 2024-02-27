6ee2a385c88ba19fad19f3cb11e6b37fec71a933
makdad-raee <makdadraeeeeee@gmail.com>
2024-02-27 12:31:52 -0800

    edit UI

--- lib/features/data/presentation/views/feeds/feeds_body.dart
+++ lib/features/data/presentation/views/feeds/feeds_body.dart
@@ -1,7 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:makdad_app/core/utils/componants.dart';
+import 'package:makdad_app/core/utils/constant.dart';
 import 'package:makdad_app/features/data/presentation/manger/social_cubit/social_cubit.dart';
 import 'package:makdad_app/features/data/presentation/manger/social_cubit/social_state.dart';
 import 'package:makdad_app/features/data/presentation/views/feeds/build_bost_item.dart';
@@ -20,41 +22,7 @@ class FeedsBody extends StatelessWidget {
         var userModel = SocialCubit.get(context).usermodel;
         return Column(
           children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8),
-              child: Container(
-                padding: const EdgeInsets.only(
-                  top: 12,
-                  left: 12,
-                ),
-                height: 60,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  boxShadow: const [
-                    BoxShadow(
-                        color: Color.fromRGBO(224, 224, 224, 1),
-                        blurRadius: 6,
-                        offset: Offset(0, 0)),
-                    BoxShadow(
-                        color: Colors.white,
-                        blurRadius: 6,
-                        offset: Offset(2, 0))
-                  ],
-                  border: Border.all(
-                      width: 2, color: const Color.fromRGBO(238, 238, 238, 1)),
-                  borderRadius: BorderRadiusDirectional.circular(40),
-                  color: Colors.grey[200],
-                ),
-                child: Text(
-                  'Whats in your mind ?',
-                  style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.grey[500],
-                      fontStyle: FontStyle.italic),
-                  textAlign: TextAlign.start,
-                ),
-              ),
-            ),
+            const StoryAndWritePostView(),
             ListView.builder(
                 shrinkWrap: true,
                 physics: const NeverScrollableScrollPhysics(),
@@ -67,6 +35,129 @@ class FeedsBody extends StatelessWidget {
           ],
         );
       },
+    );
+  }
+}
+
+class StoryAndWritePostView extends StatelessWidget {
+  const StoryAndWritePostView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8),
+      child: Column(
+        //crossAxisAlignment: CrossAxisAlignment.stretch,
+
+        children: [
+          Container(
+            padding: const EdgeInsets.only(
+              top: 12,
+              left: 12,
+            ),
+            height: 60,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              boxShadow: const [
+                BoxShadow(
+                    color: Color.fromRGBO(224, 224, 224, 1),
+                    blurRadius: 6,
+                    offset: Offset(0, 2)),
+                BoxShadow(
+                    color: Colors.white, blurRadius: 6, offset: Offset(2, 0))
+              ],
+              border: Border.all(
+                  width: 2, color: const Color.fromRGBO(224, 224, 224, 1)),
+              borderRadius: BorderRadiusDirectional.circular(40),
+              color: Colors.grey[200],
+            ),
+            child: Text(
+              'Whats in your mind ?',
+              style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.grey[500],
+                  fontStyle: FontStyle.italic),
+              textAlign: TextAlign.start,
+            ),
+          ),
+          // const SizedBox(
+          //   height: 12,
+          // ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 6),
+            child: Row(
+              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Container(
+                      height: 50,
+                      decoration: BoxDecoration(
+                          color: Colors.red.withOpacity(0.8),
+                          borderRadius: BorderRadius.circular(18)),
+                      child: const Center(
+                          child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            FontAwesomeIcons.video,
+                            color: Colors.white,
+                          ),
+                          SizedBox(
+                            width: 12,
+                          ),
+                          Text(
+                            'live',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ))),
+                ),
+                const SizedBox(
+                  width: 12,
+                ),
+                //Spacer(),
+                Expanded(
+                  child: Container(
+                      height: 50,
+                      decoration: BoxDecoration(
+                          color: defaultColor.withOpacity(0.8),
+                          borderRadius: BorderRadius.circular(18)),
+                      child: const Center(
+                          child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            FontAwesomeIcons.photoFilm,
+                            color: Colors.white,
+                          ),
+                          SizedBox(
+                            width: 12,
+                          ),
+                          Text(
+                            'Photos',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ))),
+                ),
+                const SizedBox(
+                  width: 8,
+                ),
+                Container(
+                    width: 60,
+                    height: 40,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.grey[300]),
+                    child: IconButton(
+                        onPressed: () {}, icon: const Icon(Icons.more_horiz))),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
