51d6d615933ccea396160b2895b0cf0a6efd0e24
makdad-raee <makdadraeeeeee@gmail.com>
2024-03-16 12:55:03 -0700

    add user info ui and freinds not finished

--- lib/features/data/presentation/views/users/userdetails.dart
+++ lib/features/data/presentation/views/users/userdetails.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:makdad_app/core/utils/componants.dart';
 import 'package:makdad_app/core/utils/constant.dart';
 import 'package:makdad_app/features/data/models/post_model.dart';
 import 'package:makdad_app/features/data/models/user_model.dart';
@@ -7,7 +9,6 @@ import 'package:makdad_app/features/data/presentation/manger/social_cubit/social
 import 'package:makdad_app/features/data/presentation/manger/social_cubit/social_state.dart';
 import 'package:makdad_app/features/data/presentation/views/feeds/build_bost_item.dart';
 import 'package:makdad_app/features/data/presentation/views/settings/profile_pic_bio_name.dart';
-import 'package:makdad_app/features/data/presentation/views/settings/settings.dart';
 
 class UserDetailsProfile extends StatelessWidget {
   const UserDetailsProfile(
@@ -42,31 +43,15 @@ class UserDetailsProfile extends StatelessWidget {
                 const SizedBox(
                   height: 12,
                 ),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Row(
-                      children: [
-                        Text(
-                          '2.8k ',
-                          style: style16,
-                        ),
-                        Text(
-                          'Followers',
-                          style: style16,
-                        ),
-                      ],
-                    ),
-                    // Text(
-                    //   '850 Friends',
-                    //   style: style16,
-                    // ),
-                  ],
-                ),
+                const FlowersAndUserCount(),
                 const Padding(
                   padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
                   child: AddFriendANdMessages(),
                 ),
+                SizedBox(
+                  height: 12,
+                ),
+                const VisitedUserInformation(),
                 listofvisituserspost.isNotEmpty
                     ? ListView.builder(
                         physics: const NeverScrollableScrollPhysics(),
@@ -83,6 +68,218 @@ class UserDetailsProfile extends StatelessWidget {
           ),
         );
       },
+    );
+  }
+}
+
+class FlowersAndUserCount extends StatelessWidget {
+  const FlowersAndUserCount({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Row(
+          children: [
+            const Text(
+              '2.8k ',
+              style: style18,
+            ),
+            Text(
+              'Followers',
+              style: style16.copyWith(fontWeight: FontWeight.w400),
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            const Text(
+              '2.8k ',
+              style: style18,
+            ),
+            Text(
+              'Friends',
+              style: style16.copyWith(fontWeight: FontWeight.w400),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class AddFriendANdMessages extends StatelessWidget {
+  const AddFriendANdMessages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Container(
+            height: 50,
+            // width: 80,
+            decoration: BoxDecoration(
+                color: defaulColor, borderRadius: BorderRadius.circular(24)),
+            child: const Center(
+                child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(
+                  FontAwesomeIcons.userPlus,
+                  color: Colors.white,
+                ),
+                Text(
+                  'Add Friend',
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+              ],
+            )),
+          ),
+        ),
+        const SizedBox(
+          width: 12,
+        ),
+        Expanded(
+          child: Container(
+            height: 50,
+            // width: 80,
+            decoration: BoxDecoration(
+                color: Colors.grey.withOpacity(0.4),
+                borderRadius: BorderRadius.circular(24)),
+            child: const Center(
+                child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(FontAwesomeIcons.commentDots),
+                Text(
+                  'Messages',
+                  style: TextStyle(color: Colors.black, fontSize: 18),
+                ),
+              ],
+            )),
+          ),
+        ),
+        const SizedBox(
+          width: 12,
+        ),
+        Container(
+            height: 40,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.grey[300]),
+            child: Center(
+              child: IconButton(
+                  onPressed: () {}, icon: const Icon(Icons.more_horiz)),
+            )),
+
+        //
+      ],
+    );
+  }
+}
+
+class VisitedUserInformation extends StatelessWidget {
+  const VisitedUserInformation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: double.infinity,
+          height: 150,
+          decoration: BoxDecoration(color: Colors.grey.withOpacity(0.2)),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  // mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Icon(
+                      Icons.location_on,
+                      color: Colors.grey.withOpacity(0.8),
+                    ),
+                    const Text(
+                      'From',
+                      style: style18,
+                    ),
+                    const SizedBox(
+                      width: 12,
+                    ),
+                    Text('Syria , Lattakia',
+                        style: style16.copyWith(fontWeight: FontWeight.w400)),
+                  ],
+                ),
+                Row(
+                  // mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Icon(
+                      FontAwesomeIcons.instagram,
+                      color: Colors.grey.withOpacity(0.8),
+                    ),
+                    const SizedBox(
+                      width: 12,
+                    ),
+                    const Text('Hasan Hayek', style: style18),
+                  ],
+                ),
+                Row(
+                  // mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Icon(
+                      Icons.person,
+                      color: Colors.grey.withOpacity(0.8),
+                    ),
+                    const SizedBox(
+                      width: 12,
+                    ),
+                    Text('See full Hasan Hayek \'s about info',
+                        style: style16.copyWith(fontWeight: FontWeight.w500)),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Friends',
+                    style: TextStyle(fontWeight: FontWeight.w600, fontSize: 30),
+                  ),
+                  Text('804 , 23 Mutual Friends',style: TextStyle(color: Colors.grey,))
+                ],
+              ),
+              const Spacer(),
+              Container(
+                height: 50,
+                decoration: BoxDecoration(
+                    color: Colors.grey.withOpacity(0.2),
+                    borderRadius: BorderRadius.circular(30)),
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+                  child: Center(
+                      child: Text(
+                    'See all Friends',
+                    style: TextStyle(fontSize: 16),
+                  )),
+                ),
+              )
+            ],
+          ),
+        )
+      ],
     );
   }
 }
